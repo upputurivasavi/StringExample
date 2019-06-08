08d54114d46b9e6001961aec058e9f915beebfc3
upputurivasavi <upputuri.vasavi@gmail.com>
2019-06-08 17:56:27 +0530

    updated deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,10 +3,10 @@
 export env=$1
 export version=$2
 JAR_NAME=/home/travis/build/upputurivasavi/StringExample/target/StringExample-${version}.jar
-CLASS_NAME="com.examples.Arithmaticoperations"
+CLASS_NAME="com.examples.StringExample"
 
 if [ ${env} = "dev" ] || [ ${env} = "staging" ] || [ ${env} = "prod" ]; then 
-    java -cp ${JAR_NAME} ${CLASS_NAME} 3 4
+    java -cp ${JAR_NAME} ${CLASS_NAME} upputuri vasavi
     aws s3 cp ${JAR_NAME} s3://hadoop-dev-bucket/ArithmaticExamples-${version}.jar
 else 
     echo "Invalid Environment"
